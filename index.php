ebc5e8c1cc81cfbac6844a27f926457d38ad13ff
Thahaxaina12 <saugatpadney4@gmail.com>
2022-06-14 14:32:08 +0545

    2nd commit

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 <div class="logo">  
     <ul class="nav nav-pills"  style = "margin-left:40px; ">
         <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#" style="background-color:white; color:black; font-size:40px;">BuildOn</a>
+            <a class="nav-link active" aria-current="page" href="#" style="background-color:transparent; color:black; font-size:40px;">BuildOn</a>
         </li>
     <ul>
 <div class="nav-bar-down" >
@@ -102,9 +102,168 @@
         </div>
 </div>
 <div class="para">
-<a class="nepal"></a><p>Our mission is to break the cycle of poverty, illiteracy, and low expectations through </p>
-  <p>  service and education. We mobilize U.S. students to transform our communities </p>
-  <p>   through service and change the world by building schools in developing countries.</p></a>
+    <a class="nepal"></a><p>Our mission is to break the cycle of poverty, illiteracy, and low expectations through </p>
+    <p>  service and education. We mobilize U.S. students to transform our communities </p>
+    <p>   through service and change the world by building schools in developing countries.</p></a>
+</div>
+<div class="ourwork">
+        <h1 style="font-size:50px; margin-top:30px; padding-left: 30px; ">We’re Building a Movement</h1>
+        <p style="font-size:20px; float:left;padding-top:40px;line-spacing:1.5px; padding-left: 50px;">Our U.S. Service Learning Program gives students in underperforming urban <br> high schools the opportunity to impact their 
+        local communities in powerful ways. And through our Global <br> School Construction Program, our students prove  that service
+        and education can build a stronger <br> community and a better future. </p>
+        <img src="child.jpg" alt="child" style="height:300px; width:500px; float:right; padding-right:10px">
+    
+    <div style="padding-left: 120px; font-size:30px; float:left;" >
+        <button style=" background-color: orangered; margin-top:50px; color:white; position:absolute">Explore Our Work</button>
+        </div>
 </div>
 </body>
+<div class="container">
+  <div class="row" >
+    <div class="col align-self-start">
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, corporis maxime modi laudantium repudiandae cum consequatur beatae atque tempora veniam neque reprehenderit aspernatur quos totam vitae ullam nobis, voluptatem at adipisci numquam itaque, alias amet blanditiis recusandae? Quis ratione harum similique, et at eaque cum dicta dolorem nulla deserunt, veritatis eum? Mollitia recusandae impedit repudiandae iusto minus minima laboriosam fugiat neque ullam, distinctio nulla expedita? Eos quibusdam placeat quisquam asperiores?
+    </div>
+    <div class="col align-self-center">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate reprehenderit temporibus voluptatibus quaerat inventore? Facilis, aliquid! Totam eaque veniam similique fuga, id eveniet ex cumque, facilis eos quas alias nemo at, omnis velit fugiat quidem sunt ipsam unde voluptatem atque? Amet reprehenderit laudantium soluta enim doloribus. Id suscipit possimus, expedita molestiae ex est accusantium eaque accusamus molestias amet illo unde sit corporis ea inventore odio et quos dolorum cumque doloribus.
+    </div>
+    <div class="col align-self-end">
+   Lorem ipsum dolor sit amet consectetur adipisicing elit. Est nihil iure illum nesciunt suscipit, tenetur animi quis aperiam magnam quaerat saepe incidunt nisi, consectetur aliquid nemo porro quibusdam? Rerum, blanditiis? Repellat esse aspernatur eveniet odio, atque vitae harum dolores ipsa, qui necessitatibus vero quibusdam deserunt voluptatem reprehenderit fuga consequuntur nostrum! Vitae, quae animi nemo natus nihil eius dicta consectetur quas quos beatae harum incidunt iusto eum deserunt, adipisci, quo fuga?
+    </div>
+  </div>
+</div>
+<nav aria-label="Page navigation example" style="margin:50px;">
+  <ul class="pagination">
+    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  </ul>
+</nav>
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+  <!-- Section: Social media -->
+  <section
+    class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+  >
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Get connected with us on social networks:</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Company name
+          </h6>
+          <p>
+            Here you can use rows and columns to organize your footer content. Lorem ipsum
+            dolor sit amet, consectetur adipisicing elit.
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Products
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Angular</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">React</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vue</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Laravel</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful links
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Pricing</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Settings</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Orders</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Help</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Contact
+          </h6>
+          <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            info@example.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+          <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
 </html>
