fb6257c85a4dc469efb7e208175d26e5d15ad42e
Thahaxaina12 <saugatpadney4@gmail.com>
2022-06-14 13:11:35 +0545

    first day at work

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" href="style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fest nepal</title>
+
+</head>
+
+<body>
+<header>
+ <div class="right-bar-top">  
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link active" style="color:orange " aria-current="page" href="#" >Translation Site   <span>|</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" >France</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nepali</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">English</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Chinese</a>
+            </li>
+        </ul>
+</div>
+<div class="logo">  
+    <ul class="nav nav-pills"  style = "margin-left:40px; ">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#" style="background-color:white; color:black; font-size:40px;">BuildOn</a>
+        </li>
+    <ul>
+<div class="nav-bar-down" >
+        <ul class="nav justify-content-end " style = "margin-left:740px; margin-top:-60px">
+            <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" style="background-color:white; color:black" >
+        <b> Our Work</b>
+        </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+        <li><a class="dropdown-item" href="#">Action</a></li>
+        <li><a class="dropdown-item" href="#">Another action</a></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" style="margin-left:30px;background-color:white; color:black">
+    <b> Get Involved</b>
+    </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+        <li><a class="dropdown-item" href="#">Action</a></li>
+        <li><a class="dropdown-item" href="#">Another action</a></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>
+    </ul>
+  <button class="btn btn-secondary " style="margin-left:30px; background-color:white; color:black">
+   <b>Events</b>
+  </button>
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" style="margin-left:30px; background-color:white; color:black">
+   <b>About Us</b>
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+        <li><a class="dropdown-item" href="#">Action</a></li>
+        <li><a class="dropdown-item" href="#">Another action</a></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+  <button type="submit" style="margin-left:30px; background-color:red; color:white" >DONATE</button>
+</ul>
+</div>
+</div>
+</header>
+    <div class="hero">
+        <div class="centered">
+                <a style="font-size: 40px"><p>We build hope.</p>
+                        <p>We buildOn<br></p>
+            <button style="background-color:orange; font ">Join us</button></a>
+         </div>
+</div>
+<div class="form">
+        <p>Never miss a chance to make a difference</p>
+        <div class="form-center">
+            <form class="row gx-3 gy-2 align-items-center">
+                    <div class="col-sm-3" style="margin-left: 300px;">
+                        <label class="visually-hidden" for="specificSizeInputName" >Name</label>
+                        <input type="text" class="form-control" id="specificSizeInputName" placeholder="First name">
+                    </div>
+                    <div class="col-sm-3">
+                        <label class="visually-hidden" for="specificSizeInputGroupUsername">Username</label>
+                        <div class="input-group">
+                        <input type="text" class="form-control" id="specificSizeInputGroupUsername" placeholder="Email">
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                        <button type="submit" style="background-color:transparent; color:white; font-weight:20px; border-color:white;   margin-bottom:20px ;" class="btn btn-primary">Submit</button>
+                    </div>
+            </form>
+        </div>
+</div>
+<div class="para">
+<a class="nepal"></a><p>Our mission is to break the cycle of poverty, illiteracy, and low expectations through </p>
+  <p>  service and education. We mobilize U.S. students to transform our communities </p>
+  <p>   through service and change the world by building schools in developing countries.</p></a>
+</div>
+</body>
+</html>
